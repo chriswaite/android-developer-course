2d4000dd9db5ad61c0de0f11a3546492eff657ed
Christopher Waite <ChristopherWaite@rentalcars.com>
2020-10-15 06:38:06 +0100

    commit for all so far

--- app/src/main/java/com/example/exampleapp/MainActivity.kt
+++ app/src/main/java/com/example/exampleapp/MainActivity.kt
@@ -4,15 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    fun clickFunction(view: View) {
-        Log.i("Info", "Button pressed!")
+    fun loginFunction(view: View) {
+
+        var usernameInput: EditText = findViewById(R.id.usernameFieldText)
+        var passwordInput: EditText = findViewById(R.id.passwordFieldText)
+
+        Log.i("Info", "Login Button pressed")
+        Log.i("Username", usernameInput.text.toString())
+        Log.i("Password", passwordInput.text.toString())
+
+        Toast.makeText(this, "Welcome back " + usernameInput.text.toString(), Toast.LENGTH_SHORT).show()
     }
 
-    fun loginFunction(view: View) {
-        Log.i("Info", "Login Button pressed")
+    fun switchImageButton(view: View) {
+        val image: ImageView = findViewById(R.id.imageViewer)
+        image.setImageResource(R.drawable.dog)
+
+        Toast.makeText(this, "Image switched", Toast.LENGTH_SHORT).show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
